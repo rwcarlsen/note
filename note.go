5876f976aa75294e26bf09cc483774b45507a656
Robert Carlsen <rwcarlsen@gmail.com>
2013-11-12 15:58:27 -0600

    added meta field to notes table

--- note.go
+++ note.go
@@ -6,6 +6,8 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"os"
+	"path/filepath"
 	"time"
 
 	_ "code.google.com/p/go-sqlite/go1/sqlite3"
@@ -21,22 +23,27 @@ var (
 const (
 	notesTable = "notes"
 	timeField  = "unixtime"
+	metaField  = "meta"
 	dataField  = "data"
 )
 
 func main() {
 	flag.Parse()
 
-	// determine note data
+	if flag.NArg() > 1 {
+		log.Fatal("only one arg must be provided")
+	}
+
+	// get file and/or meta data
 	var err error
-	var data []byte
+	data := []byte{}
+	meta := ""
 	if *file != "" {
 		data, err = ioutil.ReadFile(*file)
 		fatalif(err)
-	} else if flag.NArg() == 1 {
-		data = []byte(flag.Arg(0))
-	} else {
-		log.Fatal("one and only one arg must be provided")
+	}
+	if flag.NArg() == 1 {
+		meta = flag.Arg(0)
 	}
 
 	// open database
@@ -45,17 +52,17 @@ func main() {
 	defer db.Close()
 
 	// create table
-	sql := fmt.Sprintf("CREATE TABLE IF NOT EXISTS %v (%v INTEGER, %v BLOB);",
-		notesTable, timeField, dataField)
+	sql := fmt.Sprintf("CREATE TABLE IF NOT EXISTS %v (%v INTEGER, %v TEXT, %v BLOB);",
+		notesTable, timeField, metaField, dataField)
 	_, err = db.Exec(sql)
 	fatalif(err)
 
 	// insert note into db
-	sql = fmt.Sprintf("INSERT INTO %v VALUES (?,?);", notesTable)
-	_, err = db.Exec(sql, time.Now().Unix(), data)
+	sql = fmt.Sprintf("INSERT INTO %v VALUES (?,?,?);", notesTable)
+	_, err = db.Exec(sql, time.Now().Unix(), meta, data)
 	fatalif(err)
 
-	fmt.Printf("Data dumped successfully to '%v'\n", dbname)
+	fmt.Printf("Data dumped successfully to '%v'\n", *dbpath)
 }
 
 func fatalif(err error) {
