a74728a08c9f1c451c9604c387a47ad5e598ca37
Robert Carlsen <rwcarlsen@gmail.com>
2013-11-13 16:05:02 -0600

    added flag to query database with sql

--- note.go
+++ note.go
@@ -8,6 +8,7 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"strings"
 	"time"
 
 	_ "code.google.com/p/go-sqlite/go1/sqlite3"
@@ -16,6 +17,9 @@ import (
 var defaultdb = filepath.Join(os.Getenv("HOME"), "personal-notes.sqlite")
 
 var (
+	query  = flag.String("q", "", "run single arg as sql query and print output")
+	delim  = flag.String("delim", "", "row separator when using the query flag")
+	sep    = flag.String("sep", "\t", "row separator when using the query flag")
 	file   = flag.String("f", "", "use file contents as the note text")
 	dbpath = flag.String("db", getdb(), "path to notes database")
 )
@@ -39,12 +43,42 @@ const (
 func main() {
 	log.SetFlags(0)
 	flag.Parse()
-	if flag.NArg() > 2 || flag.NArg() == 0 {
-		log.Fatal("1 or 2 args required: meta [note]")
+
+	// open database
+	db, err := sql.Open("sqlite3", *dbpath)
+	fatalif(err)
+	defer db.Close()
+
+	// run query
+	if *query != "" {
+		rows, err := db.Query(*query)
+		fatalif(err)
+		cols, _ := rows.Columns()
+		rw := make([]interface{}, len(cols))
+		for i := range rw {
+			tmp := ""
+			rw[i] = &tmp
+		}
+		for rows.Next() {
+			err := rows.Scan(rw...)
+			fatalif(err)
+			row := make([]string, len(rw))
+			for i := range rw {
+				row[i] = *(rw[i]).(*string)
+			}
+			fmt.Print(strings.Join(row, *sep), "\n", *delim)
+			if *delim != "" {
+				fmt.Print("\n")
+			}
+		}
+		fatalif(rows.Err())
+		return
 	}
 
 	// get file and/or meta data
-	var err error
+	if flag.NArg() > 2 || flag.NArg() == 0 {
+		log.Fatal("1 or 2 args required: meta [note] | sql")
+	}
 	data := []byte{}
 	meta := flag.Arg(0)
 	if *file != "" {
@@ -61,11 +95,6 @@ func main() {
 			data = []byte(flag.Arg(1))
 		}
 	}
-
-	// open database
-	db, err := sql.Open("sqlite3", *dbpath)
-	fatalif(err)
-	defer db.Close()
 
 	// create table
 	sql := fmt.Sprintf("CREATE TABLE IF NOT EXISTS %v (%v INTEGER, %v TEXT, %v BLOB);",
